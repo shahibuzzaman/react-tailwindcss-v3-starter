62ece4c05d5795d8f54bb507a549938965ddf597
CloneStudio <szshishir.bd@gmail.com>
2021-12-14 12:35:46 +0600

    template-created

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-5xl bg-yellow-300 font-bold underline">Hello!</h1>
+    </div>
+  );
+};
+
+export default App;
